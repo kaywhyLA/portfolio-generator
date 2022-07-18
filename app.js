1c181378e805f0aefd454230accf48f33cea705f
KHAYLLA HITE <KHAYLLAHOUSTON@YAHOO.COM>
2022-07-18 17:48:00 -0500

    import inquirer into app.js

--- app.js
+++ app.js
@@ -1,16 +1,20 @@
-const fs = require('fs');
+const inquirer = require('inquirer');
 
-const generatePage = require('./src/page-template.js');
+inquirer
+    .prompt([{
+        type: 'input',
+        name: 'name',
+        message: 'What is your name?'
+    }])
+    .then(answers => console.log(answers));
 
-const profileDataArgs = process.argv.slice(2);
+//const fs = require('fs');
 
-console.log(profileDataArgs);
+//const generatePage = require('./src/page-template.js');
 
-const [name, github] = profileDataArgs;
+//const pageHTML = generatePage(name, github);
 
-const pageHTML = generatePage(name, github);
-
-fs.writeFile('./index.html', generatePage(name, github), err => {
-    if (err) throw new Error(err);
-    console.log('Portfolio complete! Check out index.html to see the output!');
-});
+//fs.writeFile('./index.html', generatePage(name, github), err => {
+//if (err) throw new Error(err);
+//console.log('Portfolio complete! Check out index.html to see the output!');
+//});
