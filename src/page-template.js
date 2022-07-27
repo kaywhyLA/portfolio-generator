2c71433bf3c91a5692c165307f85e57e8eb65a53
KHAYLLA HITE <KHAYLLAHOUSTON@YAHOO.COM>
2022-07-27 10:42:23 -0500

    updated page-template.js and app.js

--- src/page-template.js
+++ src/page-template.js
@@ -12,56 +12,53 @@ const generateAbout = aboutText => {
 };
 
 // create the projects section 
-const generateProjects = projectArr => {
+const generateProjects = projectsArr => {
     return `
     <section class="my-3" id="portfolio">
     <h2 class="text-dark bg-primary p-2 display-inline-block">Work</h2>
     <div class="flex-row justify-space-between">
-    ${projectsArr
+    ${ projectsArr
     .filter(({feature}) => feature) 
-.map(({name,description, languages, link}) => {
-    return
-    ` <
+.map(({ name, description, languages, link}) => {
+    return ` <
         div class = "col-12 mb-2 bg-dark text-light p-3" >
         <
         h3 class = "portfolio-item-title text-light" > $ { name } < /h3> <
-        h5 class = "portfolio languages" >
+    h5 class = "portfolio languages" >
         Built With:
         $ { languages.map(languages => language).join(',') } <
         /h5> <
-        p > $ { description } < /p> <
-        a href "${link}"
+    p > $ { description } < /p> <
+    a href "${link}"
     class = "btn" > < i class = "fab fa-github mr-2" > < /i>View Project on Github</a >
         <
         /div>
     `;
 })
 .join('')}
-    ${projectsArr
-    .filter(({feature}) => !feature)
-    .map(({name, description, languages, link}) => {
-        return 
-        ` <
+
+${projectsArr
+  .filter(({ feature }) => !feature)
+  .map(({ name, description, languages, link }) => {
+    return ` <
     div class = "col-12 col-md-6 mb-2 bg-dark text-light p-3 flex-column" >
         <
         h3 class = "portfolio-item-title text-light" > $ { name } < /h3> <
         h5 class = "portfolio-languages" >
         Built With:
-        $ { languages.join(',') } <
-        h5 >
-        <
+        $ { languages.join(', ') } <
+        /h5> <
         p > $ { description } < /p> <
         a href = "${link}"
-    class = "btn mt-auto" > < i class = "fab fa-github mr-2" > < /i>View Project on Github</a >
+    class = "btn mt-auto" > < i class = "fab fa-github mr-2" > < /i>View Project on GitHub</a >
         <
         /div>
     `;
-
-    })
-    .join('')}
-    </div>
-    </section>
-    `;
+  })
+  .join('')}
+</div>
+</section>
+`;
 };
 
 // export function to generate entire page
